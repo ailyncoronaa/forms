79fa4d9d4e20e70618ada5c9b0590b6aee2ddb11
Ailyn Morales <ailyncoronaa@gmail.com>
2026-03-27 22:32:00 -0400

    added users list and quiz route

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,43 @@
 const express = require('express');
 const router = express.Router();
 
-const users = [{ firstName: "Kyle" }, { firstName: "Sally" }];
-
-router.get('/', (req, res) => {
+router.route('/').get((req, res) => {
     res.send('User List');
+     }).post((req, res) =>{
+        const firstName = req.body.firstName;
+        const isValid = firstName !=="";
+    if (isValid){
+        console.log(`Adding user: ${firstName}`);
+        users.push({firstName});
+        res.render('users/list', {users});
+    }
+    else{
+        console.log("Error adding user!");
+        res.render("users/new", {firstName: firstName});
+    }
 });
-
+router.get("/list", (req, res) =>{
+    res.render("users/list", {users});
+});
 router.get('/new', (req, res) => {
-    res.render('users/new');
+    res.render('users/new', { firstName: "Test" })
+});
+// router.get("/:id", (req, res) =>{ 
+//     res.send('Getting User data : ${req.params.id}');
+// });
+router.route("/:id").get((req, res) =>{
+    console.log(req.user);
+    console.log('Getting User Data!')
+    res.send(`Getting User data for id : ${req.params.id}`);
+}).delete((req, res) =>{
+    res.send(`Deleting User data for id : ${req.params.id}`);
+}).put((req, res) =>{
+    res.send(`Updating User data for id : ${req.params.id}`);
 });
 
-router.post('/', (req, res) => {
-    users.push({ firstName: req.body.firstName });
-    res.send(req.body.firstName);
+const users = [{firstName:"Jake"}, {firstName:" Judy"}];
+router.param("id", (req, res, next, id) =>{
+req.user = users[id];
+next();
 });
-
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const userRouter = require('./routes/user');
 const quizRouter = require('./routes/quiz');
+const usersRouter = require('./routes/users');
 app.use('/user', userRouter);
 
 app.set('view engine', 'ejs');
@@ -11,6 +12,7 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/quiz', quizRouter);
+app.use('/users', usersRouter);
 
 // GET /submit - logs query parameters
 app.get('/solution', (req, res) => {
